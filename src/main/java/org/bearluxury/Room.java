12957c29b9c3cca17da09ec17b2d72d8b935257d
nameofme01 <69026352+nameofme01@users.noreply.github.com>
2024-03-18 17:13:56 -0500

    added getters and setters for BED_TYPE, QUALITY_LEVEL, and ROOM_TYPE and added numberOfBeds

--- src/main/java/org/bearluxury/Room.java
+++ src/main/java/org/bearluxury/Room.java
@@ -32,15 +32,41 @@ class Room {
     private ROOM_TYPE roomType;
     private BED_TYPE bed;
     private QUALITY_LEVEL qualityLevel;
+    private int numberOfBeds;
 
     public Room(int roomNumber, double price, boolean canSmoke,
-                ROOM_TYPE roomType, BED_TYPE bed, QUALITY_LEVEL qualityLevel) {
+                ROOM_TYPE roomType, BED_TYPE bed, QUALITY_LEVEL qualityLevel, int numberOfBeds) {
         this.roomNumber = roomNumber;
         // this.isClean = isClean;
         this.canSmoke = canSmoke;
         this.price = price;
         this.roomType = roomType;
         this.bed = bed;
+        this.qualityLevel = qualityLevel;
+        this.numberOfBeds = numberOfBeds;
+    }
+
+    public ROOM_TYPE getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(ROOM_TYPE roomType) {
+        this.roomType = roomType;
+    }
+
+    public BED_TYPE getBed() {
+        return bed;
+    }
+
+    public void setBed(BED_TYPE bed) {
+        this.bed = bed;
+    }
+
+    public QUALITY_LEVEL getQualityLevel() {
+        return qualityLevel;
+    }
+
+    public void setQualityLevel(QUALITY_LEVEL qualityLevel) {
         this.qualityLevel = qualityLevel;
     }
 
@@ -68,6 +94,11 @@ class Room {
         this.price = price;
     }
 
+    public int getNumberOfBeds() {
+        return numberOfBeds;
+    }
 
-
+    public void setNumberOfBeds(int numberOfBeds) {
+        this.numberOfBeds = numberOfBeds;
+    }
 }
