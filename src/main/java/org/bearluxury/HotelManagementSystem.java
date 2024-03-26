1716f34ab3d2eea932826825f78c4e19c38f4db7
willclo1 <115596953+willclo1@users.noreply.github.com>
2024-03-26 13:42:09 -0500

    STUFF

--- src/main/java/org/bearluxury/HotelManagementSystem.java
+++ src/main/java/org/bearluxury/HotelManagementSystem.java
@@ -14,6 +14,7 @@ import java.awt.event.ActionListener;
 import java.time.LocalDate;
 
 //Window
+
 public class HotelManagementSystem extends JFrame implements DateChangeListener, ActionListener {
 
     ImageIcon icon;
