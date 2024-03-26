83286d23d1342d16c3572f46c01e6390e51ff545
willclo1 <115596953+willclo1@users.noreply.github.com>
2024-03-26 14:52:52 -0500

    Revert "FIXING"
    This reverts commit 30aad6aa2f3417f665dfc2f47f2162fffaa4edc9.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,1 @@
-insert into reservations (room_number, guest_name, start_date, end_date, discount) VALUES (100, 'bill', '2024-03-25', '2024-03-30', 1.0);
+insert into RESERVATIONDB.PUBLIC.RESERVATIONS (room_number, guest_name, start_date, end_date, discount) VALUES (100, 'bill', '2024-03-25', '2024-03-30', 1.0);
