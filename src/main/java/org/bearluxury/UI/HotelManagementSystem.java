7be541fd0a16d0fbf1b8c352b7a367f4361febdd
alanvillagrand <116222100+alanvillagrand@users.noreply.github.com>
2024-04-16 18:38:12 -0500

    Fixed HotelManagementSystem issue

--- src/main/java/org/bearluxury/UI/HotelManagementSystem.java
+++ src/main/java/org/bearluxury/UI/HotelManagementSystem.java
@@ -2,15 +2,8 @@ package org.bearluxury.UI;
 
 import com.formdev.flatlaf.FlatLightLaf;
 import com.formdev.flatlaf.fonts.roboto.FlatRobotoFont;
-import org.bearluxury.account.AccountBuilder;
+import org.bearluxury.account.Role;
 import org.bearluxury.controllers.RoomController;
-import org.bearluxury.product.ProductBuilder;
-import org.bearluxury.product.ProductCatalog;
-import org.bearluxury.reservation.Reservation;
-import org.bearluxury.reservation.ReservationBuilder;
-import org.bearluxury.reservation.ReservationJDBCDAO;
-import org.bearluxury.room.Room;
-import org.bearluxury.room.RoomBuilder;
 import org.bearluxury.room.RoomCatalog;
 import org.bearluxury.room.RoomJDBCDAO;
 
@@ -18,28 +11,45 @@ import javax.swing.*;
 import java.awt.*;
 import java.sql.SQLException;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+
 
 //Window
 public class HotelManagementSystem  {
 
-    public static void openRoomCatalogPane(int beds, LocalDate checkIn, LocalDate checkOut){
+    public static void openRoomCatalogPane(int beds, LocalDate checkIn, LocalDate checkOut, Role role){
         try {
             RoomController rooms = new RoomController(new RoomJDBCDAO());
             RoomCatalog roomCatalog = new RoomCatalog();
             roomCatalog.setRooms(rooms.listRooms());
-            AvaliableRoomsGUI catalogPane = new AvaliableRoomsGUI(roomCatalog, beds, checkIn, checkOut);
+            AvaliableRoomsGUI catalogPane = new AvaliableRoomsGUI(roomCatalog, beds, checkIn, checkOut, role);
             catalogPane.setVisible(true);
         }catch (SQLException exc){
             exc.printStackTrace();
         }
     }
 
+    //added homepages for user role
+    /*
     public static void openHomePage() {
         HotelHomePage hotelHomePage = new HotelHomePage();
         hotelHomePage.setVisible(true);
+    }
+
+     */
+
+    public static void openGuestHomePage() {
+        GuestHomePage guestHomePage = new GuestHomePage(Role.GUEST);
+        guestHomePage.setVisible(true);
+    }
+
+    public static void openClerkHomePage() {
+        ClerkHomePage clerkHomePage = new ClerkHomePage(Role.CLERK);
+        clerkHomePage.setVisible(true);
+    }
+
+    public static void openAdminHomePage() {
+        AdminHomePage adminHomePage = new AdminHomePage(Role.ADMIN);
+        adminHomePage.setVisible(true);
     }
 
     public static void openLoginPage() {
@@ -52,8 +62,8 @@ public class HotelManagementSystem  {
         registerPage.setVisible(true);
     }
 
-    public static void openHotelManagmentSystem(){
-        InfoFilterPane window = new InfoFilterPane();
+    public static void openHotelManagmentSystem(Role role){
+        InfoFilterPane window = new InfoFilterPane(role);
         window.setVisible(true);
     }
 
@@ -62,19 +72,10 @@ public class HotelManagementSystem  {
         register.setVisible(true);
     }
 
-    public static void openShopHomePage() {
-        ProductCatalog productCatalog = new ProductCatalog();
-        ProductBuilder productBuilder = new ProductBuilder("src/main/resources/ProductList.csv");
-        System.out.println(productBuilder.getProductList().size());
-        productCatalog.setProducts(productBuilder.getProductList());
-        ShopHomePage shopHomePage = new ShopHomePage(productCatalog);
-        shopHomePage.setVisible(true);
-    }
-
     public static void main(String[] args) {
         FlatRobotoFont.install();
-        UIManager.put("defaultFont", Style.defaultFont);
+        UIManager.put("defaultFont", new Font(FlatRobotoFont.FAMILY, Font.PLAIN, 13));
         FlatLightLaf.setup();
-        openShopHomePage();
+        openLoginPage();
     }
 }
