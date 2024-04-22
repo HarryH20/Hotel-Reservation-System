760e79c1c10442a2cea384741f43793f91e0cf25
Joseph Zuniga <josephzuniga1993@gmail.com>
2024-04-21 21:16:59 -0500

    Made abstract class Payment and concrete class CreditCardPayment.

---
+++ src/main/java/CreditCardPayment.java
@@ -0,0 +1,18 @@
+import org.bearluxury.Payment;
+
+public class CreditCardPayment extends Payment {
+    private String cardNumber;
+    private String expiryDate;
+    private String cvv;
+
+    public CreditCardPayment(int paymentId, double amount, String cardNumber, String expiryDate, String cvv) {
+        super(paymentId, amount);
+        this.cardNumber = cardNumber;
+        this.expiryDate = expiryDate;
+        this.cvv = cvv;
+    }
+
+    @Override
+    public void processPayment() {
+    }
+}
---
+++ src/main/java/org/bearluxury/Payment.java
@@ -0,0 +1,29 @@
+package org.bearluxury;
+
+import java.util.Date;
+
+public abstract class Payment {
+    private int paymentId;
+    private Date paymentDate;
+    private double amount;
+
+    public Payment(int paymentId, double amount) {
+        this.paymentId = paymentId;
+        this.paymentDate = new Date(); // Current date and time
+        this.amount = amount;
+    }
+
+    public int getPaymentId() {
+        return paymentId;
+    }
+
+    public Date getPaymentDate() {
+        return paymentDate;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public abstract void processPayment();
+}
