5a42d888dcc7e6532538b36fe8405e84ce8a4983
nameofme01 <69026352+nameofme01@users.noreply.github.com>
2024-04-26 16:53:09 -0500

    changed recipient name for test

--- src/test/java/Email/EmailSenderTest.java
+++ src/test/java/Email/EmailSenderTest.java
@@ -8,6 +8,6 @@ import org.junit.Test;
 public class EmailSenderTest {
     @Test
     public void sendingEmailTest(){
-        EmailSender.sendConfirmationEmail("Nick","will_clore1@baylor.edu");
+        EmailSender.sendConfirmationEmail("Will","will_clore1@baylor.edu");
     }
 }
