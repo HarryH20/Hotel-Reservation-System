e2c2092b99e1eb746a270f68238fc114fe17b7ac
willclo1 <115596953+willclo1@users.noreply.github.com>
2024-04-30 16:52:45 -0500

    did controller comments

--- src/main/java/org/bearluxury/controllers/RoomController.java
+++ src/main/java/org/bearluxury/controllers/RoomController.java
@@ -7,14 +7,36 @@ import org.bearluxury.room.RoomJDBCDAO;
 import java.sql.SQLException;
 import java.util.Optional;
 import java.util.Set;
-
+/**
+ * The room controller provides methods to interact with room data in the database.
+ * It utilizes a RoomJDBCDAO object for database operations.
+ * @author Will Clore
+ * @author Harrsion Hassler
+ * @author Derek Martinez
+ * @author Nicholas Nolen
+ * @author Joseph Zuniga
+ * @author Alan Vilagrand
+ */
 public class RoomController {
     private RoomJDBCDAO roomDAO;
 
+    /**
+     * Constructs a RoomController with the specified RoomJDBCDAO and pre-fills rooms from a CSV file.
+     *
+     * @param roomDAO the RoomJDBCDAO instance to be used for database operations
+     * @throws SQLException if a database access error occurs
+     */
     public RoomController(RoomJDBCDAO roomDAO) throws SQLException {
         this.roomDAO = roomDAO;
         preFillRooms();
     }
+
+    /**
+     * Inserts a new room into the database if it doesn't already exist.
+     *
+     * @param room the room to be inserted
+     * @throws SQLException if a room with the same room number already exists in the database
+     */
     public void insertRoom(Room room) throws SQLException {
         Optional<Room> existingRoom = roomDAO.get(room.getRoomNumber());
 
@@ -27,9 +49,15 @@ public class RoomController {
             roomDAO.insert(room);
         }
     }
+
+    /**
+     * Pre-fills rooms in the database from a CSV file.
+     *
+     * @throws SQLException if a database access error occurs
+     */
     private void preFillRooms() throws SQLException {
         RoomBuilder builder = new RoomBuilder("src/main/resources/RoomList.csv");
-        for(Room room : builder.getRoomSet()){
+        for (Room room : builder.getRoomSet()) {
             Optional<Room> existingRoom = roomDAO.get(room.getRoomNumber());
 
             if (existingRoom.isPresent()) {
@@ -40,31 +68,58 @@ public class RoomController {
                 roomDAO.insert(room);
             }
         }
-
     }
 
+    /**
+     * Retrieves a room from the database based on its room number.
+     *
+     * @param roomNumber the room number of the room to retrieve
+     * @return an Optional containing the retrieved room, or empty if not found
+     */
     public Optional<Room> getRoom(int roomNumber) {
         return roomDAO.get(roomNumber);
     }
 
+    /**
+     * Retrieves a set of all rooms stored in the database.
+     *
+     * @return a Set containing all rooms stored in the database
+     */
     public Set<Room> listRooms() {
         return roomDAO.list();
     }
 
+    /**
+     * Updates an existing room in the database.
+     *
+     * @param room the updated room
+     * @param roomNumber the room number of the room to be updated
+     */
     public void updateRoom(Room room, int roomNumber) {
         roomDAO.update(room, roomNumber);
     }
 
+    /**
+     * Deletes a room from the database based on its room number.
+     *
+     * @param roomNumber the room number of the room to delete
+     * @return true if the room was successfully deleted, false otherwise
+     */
     public boolean deleteRoom(int roomNumber) {
         return roomDAO.delete(roomNumber);
     }
 
+    /**
+     * Clears all rooms from the database.
+     */
     public void clearRooms() {
         roomDAO.clear();
     }
 
+    /**
+     * Closes the connection to the database.
+     */
     public void closeConnection() {
         roomDAO.close();
     }
-
 }
--- src/main/java/org/bearluxury/controllers/SaleController.java
+++ src/main/java/org/bearluxury/controllers/SaleController.java
@@ -8,34 +8,89 @@ import org.bearluxury.shop.Sale;
 import java.sql.SQLException;
 import java.util.Optional;
 import java.util.Set;
-
+/**
+ * The sale controller provides methods to interact with sale data in the database.
+ * It utilizes a SaleJDBCDAO object for database operations.
+ *
+ * @author Will Clore
+ * @author Harrsion Hassler
+ * @author Derek Martinez
+ * @author Nicholas Nolen
+ * @author Joseph Zuniga
+ * @author Alan Vilagrand
+ */
 public class SaleController {
     private SaleJDBCDAO saleDAO;
+
+    /**
+     * Constructs a SaleController with the specified SaleJDBCDAO.
+     *
+     * @param saleDao the SaleJDBCDAO instance to be used for database operations
+     */
     public SaleController(SaleJDBCDAO saleDao){
         this.saleDAO = saleDao;
     }
+
+    /**
+     * Inserts a new sale into the database.
+     *
+     * @param sale the sale to be inserted
+     */
     public void insertSale(Sale sale) {
         try {
             saleDAO.insert(sale);
-        }catch (SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * Retrieves a set of all sales stored in the database.
+     *
+     * @return a Set containing all sales stored in the database
+     */
     public Set<Sale> listSale() {
         return saleDAO.list();
     }
 
+    /**
+     * Retrieves a set of sales associated with a specific account ID.
+     *
+     * @param acctId the account ID to retrieve sales for
+     * @return a Set containing sales associated with the specified account ID
+     */
     public Set<Sale> listSale(int acctId) {
         return saleDAO.listSalesByAccountId(acctId);
     }
 
-    public void deleteSaleByAcctId(int acctId){saleDAO.deleteSalesByAccountId(acctId);}
+    /**
+     * Deletes all sales associated with a specific account ID from the database.
+     *
+     * @param acctId the account ID to delete sales for
+     */
+    public void deleteSaleByAcctId(int acctId){
+        saleDAO.deleteSalesByAccountId(acctId);
+    }
 
-    public void deleteSaleBySaleId(int saleId){saleDAO.deleteSaleById(saleId);}
+    /**
+     * Deletes a sale from the database based on its sale ID.
+     *
+     * @param saleId the sale ID of the sale to delete
+     */
+    public void deleteSaleBySaleId(int saleId){
+        saleDAO.deleteSaleById(saleId);
+    }
+
+    /**
+     * Clears all sales from the database.
+     */
     public void clearReservation() {
         saleDAO.clear();
     }
+
+    /**
+     * Closes the connection to the database.
+     */
     public void closeConnection() {
         saleDAO.close();
     }
